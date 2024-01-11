232c7dda530fdde36d3b53f7aebcf76479f064ce
Дмитрий Шиманский <yaflay@vk.com>
2024-01-11 12:05:04 +0300

    make line system

--- App/Queue/QueueService.cs
+++ App/Queue/QueueService.cs
@@ -14,16 +14,21 @@ public class QueueService(AppDbContext ctx, IHubContext<QueueHub, IQueueHub> hub
     private Dictionary<string, List<string>> queue = provider.Queues;
     public async Task ChangeQueue(string userXUId, string gameId, ChangeQueueType type)
     {
-        switch (type)
+        var playerServer = ctx.Servers.FirstOrDefault(k => k.GameId == gameId);
+        if (playerServer == null) return;
+        var game = ctx.Games.FirstOrDefault(k => k.Id == gameId);
+        if (queue[gameId].First().Length < game.MinPlayersCount)
         {
-            case ChangeQueueType.AddUser:
-                await hub.Clients.All.ChangeQueue(gameId, queue[gameId]);
-                break;
-            case ChangeQueueType.RemoveUser:
-                await hub.Clients.All.ChangeQueue(gameId, queue[gameId]);
-                break;
-            default: break;
+            playerServer.IsInUse = false;
+            playerServer.GameId = null;
+            ctx.Servers.Update(playerServer);
+            await hub.Clients.All.StopCountdown(gameId, queue[gameId]);
         }
-        await hub.Clients.All.ChangeQueue(gameId, queue[gameId]);
+        else
+        {
+            await hub.Clients.All.StartCountdown(gameId, queue[gameId], playerServer);
+        }
+        if (queue[gameId].First().Length < game.MaxPlayersCount)
+            await hub.Clients.All.ChangeQueue(gameId, queue[gameId]);
     }
 }
